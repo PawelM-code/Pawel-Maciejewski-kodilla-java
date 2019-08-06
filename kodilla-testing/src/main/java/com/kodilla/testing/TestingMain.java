939ef4a8a739649f39b1eae935a2a3eb1142b010
PawelM-code <maciejewski.pawel@o2.pl>
2019-08-06 20:17:51 +0200

    create package and class calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+//        SimpleUser simpleUser = new SimpleUser("theForumUser");
+//
+//        String result = simpleUser.getUserName();
+//
+//        if (result.equals("theForumUser")){
+//            System.out.println("test OK");
+//        } else {
+//            System.out.println("Error!");
+//        }
+        Calculator calculator = new Calculator(2,2);
+
+        int resultAdd = calculator.getA() + calculator.getB();
+        int resultSubtract = calculator.getA() - calculator.getB();
+
+        System.out.println("Add:");
+        if(resultAdd == calculator.add(2,2)){
+            System.out.println("Test passed.");
+        } else {
+            System.out.println("Test failed.");
+        }
+
+        System.out.println("Subtract:");
+        if(resultSubtract == calculator.subtract(2,2)){
+            System.out.println("Test passed.");
+        } else {
+            System.out.println("Test failed.");
+        }
+
     }
 }
