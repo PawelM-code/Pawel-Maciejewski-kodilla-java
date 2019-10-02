f4f78ed7d1bd6d12e4a55b2637d1d83c5aae2110
PawelM-code <maciejewski.pawel@o2.pl>
2019-10-02 12:49:43 +0200

    correct task: task lists

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,27 +1,41 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BoardConfig {
-    @Bean
+    @Autowired
+    @Qualifier("toDo")
+    TaskList toDoList;
+
+    @Autowired
+    @Qualifier("progress")
+    TaskList inProgressList;
+
+    @Autowired
+    @Qualifier("done")
+    TaskList doneList;
+
+    @Bean(name = "toDo")
     public TaskList getToDoList() {
         return new TaskList();
     }
 
-    @Bean
+    @Bean(name = "progress")
     public TaskList getInProgressList() {
         return new TaskList();
     }
 
-    @Bean
+    @Bean(name = "done")
     public TaskList getDoneList() {
         return new TaskList();
     }
 
     @Bean
     public Board getBoard() {
-        return new Board(getToDoList(), getInProgressList(), getDoneList());
+        return new Board(toDoList, inProgressList, doneList);
     }
 }
