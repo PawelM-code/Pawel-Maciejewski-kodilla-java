7c1f3d30017974d2299980c6964da1a01ac6bec2
PawelM-code <maciejewski.pawel@o2.pl>
2019-10-23 19:41:32 +0200

    exercise: 17.3

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -0,0 +1,51 @@
+package com.kodilla.hibernate.manytomany;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "COMPANIES")
+public class Company {
+    private int id;
+    private String companyName;
+    private List<Employee> employeeList = new ArrayList<>();
+
+    public Company() {
+    }
+
+    public Company(String companyName) {
+        this.companyName = companyName;
+    }
+    @Id
+    @NotNull
+    @GeneratedValue
+    @Column(name = "COMPANY_ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @NotNull
+    @Column(name = "COMPANY_NAME")
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "companyList")
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+}
