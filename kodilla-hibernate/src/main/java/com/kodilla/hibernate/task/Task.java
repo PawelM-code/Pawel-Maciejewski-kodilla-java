6fd6a8703983a4f1d9d24194adf6721b1d496119
PawelM-code <maciejewski.pawel@o2.pl>
2019-10-24 21:02:20 +0200

    exercise: 17.4

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -6,6 +6,27 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
+@NamedQueries({
+        @NamedQuery(
+                name = "Task.retrieveLongTasks",
+                query = "FROM Task WHERE duration > 10"
+        ),
+        @NamedQuery(
+                name = "Task.retrieveShortTasks",
+                query = "FROM Task WHERE duration <= 10"
+        ),
+        @NamedQuery(
+                name = "Task.retrieveTasksWithDurationLongerThan",
+                query = "FROM Task WHERE duration > :DURATION"
+        )
+})
+
+@NamedNativeQuery(
+        name = "Task.retrieveTasksWithEnoughTime",
+        query = "SELECT * FROM TASKS" +
+                " WHERE DATEDIFF(DATE_ADD(CREATED, INTERVAL DURATION DAY), NOW()) > 5",
+        resultClass = Task.class
+)
 @Entity
 @Table(name = "TASKS")
 public class Task {
@@ -37,6 +58,7 @@ public class Task {
     public String getDescription() {
         return description;
     }
+
     @NotNull
     @Column(name = "CREATED")
     public Date getCreated() {
