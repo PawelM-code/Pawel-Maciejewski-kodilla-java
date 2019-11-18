bfecc2fc46d391a47bc0cd0c5f7d8537e2b495a0
PawelM-code <maciejewski.pawel@o2.pl>
2019-11-18 11:56:31 +0100

    additional task: 16.8 sudoku

---
+++ kodilla-sudoku/src/main/java/Prototype.java
@@ -0,0 +1,6 @@
+public class Prototype<T> implements Cloneable {
+    @Override
+    public T clone() throws CloneNotSupportedException {
+        return (T)super.clone();
+    }
+}
---
+++ kodilla-sudoku/src/main/java/SudokuRow.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class SudokuRow {
+    private ArrayList<SudokuElement> sudokuElements;
+
+    SudokuRow(ArrayList<SudokuElement> sudokuElements) {
+        this.sudokuElements = sudokuElements;
+    }
+
+    ArrayList<SudokuElement> getSudokuElements() {
+        return sudokuElements;
+    }
+
+    public void setSudokuElements(ArrayList<SudokuElement> sudokuElements) {
+        this.sudokuElements = sudokuElements;
+    }
+}
