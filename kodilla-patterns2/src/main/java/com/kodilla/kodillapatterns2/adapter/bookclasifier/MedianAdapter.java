a4785a9d67b97d5343c0d3f8334ec23b3aff8dc5
PawelM-code <maciejewski.pawel@o2.pl>
2019-12-13 21:42:00 +0100

    task: 24.5 adapter / omit Book class import

--- kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/adapter/bookclasifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/adapter/bookclasifier/MedianAdapter.java
@@ -1,6 +1,5 @@
 package com.kodilla.kodillapatterns2.adapter.bookclasifier;
 
-import com.kodilla.kodillapatterns2.adapter.bookclasifier.librarya.Book;
 import com.kodilla.kodillapatterns2.adapter.bookclasifier.librarya.Clasifier;
 import com.kodilla.kodillapatterns2.adapter.bookclasifier.libraryb.BookSignature;
 
@@ -10,9 +9,9 @@ import java.util.Set;
 
 public class MedianAdapter extends MedianAdaptee implements Clasifier {
     @Override
-    public int publicationYearMedian(Set<Book> bookSet) {
+    public int publicationYearMedian(Set<com.kodilla.kodillapatterns2.adapter.bookclasifier.librarya.Book> bookSet) {
         Map<BookSignature, com.kodilla.kodillapatterns2.adapter.bookclasifier.libraryb.Book> bookMap = new HashMap<>();
-        for (Book book : bookSet) {
+        for (com.kodilla.kodillapatterns2.adapter.bookclasifier.librarya.Book book : bookSet) {
             bookMap.put(new BookSignature(book.getSignature()),
                     new com.kodilla.kodillapatterns2.adapter.bookclasifier.libraryb.Book(
                             book.getAuthor(),
